d88a0efb4ede7fece9f23ca81ce63b0f2b9fc983
christophercatt <ccatt601@gmail.com>
2020-11-04 14:47:40 +0000

    Added initial utility functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const core = require("@actions/core");
+const utils = require("./utils");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    await utils.configureGit();
   } catch (error) {
     core.setFailed(error.message);
   }
