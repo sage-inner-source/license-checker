9c174976ab3b9c50905f372a652dbc43bb8cf5d6
christophercatt <ccatt601@gmail.com>
2020-11-04 13:37:46 +0000

    Initial action biolerplate setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const core = require("@actions/core");
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
